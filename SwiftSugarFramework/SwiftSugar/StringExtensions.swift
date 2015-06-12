46ffbd411bc535503a389350e64c22ca29a2a8bc
Rui Peres <rui.peres@just-eat.com>
2015-06-11 18:36:54 -0700

    General clean up

--- SwiftSugarFramework/SwiftSugar/StringExtensions.swift
+++ SwiftSugarFramework/SwiftSugar/StringExtensions.swift
@@ -10,111 +10,97 @@ import Foundation
 
 extension String {
     
-    var length :Int
-    {
+    var length :Int {
         return self.utf8.count
     }
     
-    static func join(strings: String...)->String
-    {
-        var result: String=""
-        for value in strings{
+    static func join(strings: String...) -> String {
+        var result: String = ""
+        for value in strings {
             result += value
         }
         return result
     }
     
-    static func join(array: Array<String>)->String
-    {
-        var result: String=""
-        for value in array{
+    static func join(array: Array<String>) -> String {
+        var result: String = ""
+        for value in array {
             result += value
         }
         return result
     }
     
     
-    func toCharacterArray()->[Character]{
+    func toCharacterArray() -> [Character] {
         var resultArray = [Character]()
-        
-        for char in self.characters{
+        for char in self.characters {
             resultArray.append(char)
         }
-        
         return resultArray
     }
     
     
-    func indexOfString(string : String)->Int?{
-        if self.length == 0 || string.length == 0 {
-            return nil
-        }
+    func indexOfString(string : String) -> Int? {
         
-        if self.length < string.length{
-            return nil
-        }
+        guard self.length == 0 || self.length == 0 else { return nil }
+        guard self.length < string.length else { return nil }
         
         var numberIt : Int = 0
         var selfCharArray = self.toCharacterArray()
         let stringCharArray = string.toCharacterArray()
         
-        for (index,_) in selfCharArray.enumerate(){
+        for (index,_) in selfCharArray.enumerate() {
             
-            for charIn in stringCharArray{
+            for charIn in stringCharArray {
                 
-                if charIn==selfCharArray[index+numberIt] {
+                if charIn == selfCharArray[index+numberIt] {
                     numberIt++
-                    if numberIt == string.length{
+                    if numberIt == string.length {
                         return index
                     }
                 }
-                else{
+                else {
                     numberIt=0
                     break
                 }
             }
         }
+        
+        return nil
+    }
+}
+
+extension String {
+    subscript(index: Int) -> String? {
+        var castedIndex:Int = index
+        if index < 0 {
+            castedIndex = self.length + index
+        }
+        
+        if (castedIndex > self.length || castedIndex<0){
+            return nil
+        }
+        
+        for (charIndex,charValue) in self.characters.enumerate()
+            where charIndex == castedIndex {
+                return String(charValue)
+        }
+        
         return nil
     }
     
-//    func reverse() -> String {
-//        return self.toCharacterArray().reverse().reduce("", combine: {$0 + $1})
-//    }
-}
-
-extension String{
-    subscript(index: Int) -> String? {
-        var castedIndex:Int = index
-            if index<0{
-                castedIndex = self.length + index
-            }
-            
-            if (castedIndex > self.length || castedIndex<0){
-                return nil
-            }
-            
-            for (charIndex,charValue) in self.characters.enumerate(){
-                if(charIndex == castedIndex){
-                    return String(charValue)
-                }
-            }
-            return nil
-    }
-    
-    
     subscript(startIndex: Int,endIndex: Int) -> String? {
         var returnString :String = ""
-            for (charIndex,charValue) in self.characters.enumerate(){
-                if(charIndex >= startIndex &&  charIndex <= endIndex){
-                    returnString = returnString + String(charValue)
-                }
-            }
-            
-            if returnString.isEmpty{
-                return nil
-            }
-            else{
-                return returnString
-            }
+        for (charIndex,charValue) in self.characters.enumerate()
+            where charIndex >= startIndex &&  charIndex <= endIndex {
+                returnString = returnString + String(charValue)
+        }
+        
+        if returnString.isEmpty {
+            return nil
+        }
+        else {
+            return returnString
+        }
     }
 }
